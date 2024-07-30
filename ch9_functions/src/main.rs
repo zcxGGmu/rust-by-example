d8c62a9707b692279b98d9bf548a25b8ead9be9b
Quan Zhou <zhouquan@iscas.ac.cn>
2024-07-31 00:08:50 +0800

    rbe: ch9 completed... next chapter maybe ch10 or ch21 ???

--- ch9_functions/src/main.rs
+++ ch9_functions/src/main.rs
@@ -127,6 +127,7 @@ fn closures_test() {
     as_input_parameters_test();
     type_anonymity_test();
     input_functions_test();
+    output_functions_test();
     examples_in_std_test();
 
     // &T / &mut T / T
@@ -211,25 +212,159 @@ fn closures_test() {
     }
 
     fn type_anonymity_test() {
-        
+        fn apply<F>(f: F)
+        where F: Fn() {
+            f();
+        }
+
+        let x = 8;
+        let print = || println!("{}", x);
+        apply(print);
     }
 
     fn input_functions_test() {
+        fn call_me<F: Fn()>(f: F) {
+            f()
+        }
+
+        fn function() {
+            println!("I'm a function!");
+        }        
+
+        let closure = || println!("I am a closure.");
+        call_me(closure);
+        call_me(function);
+    }
+
+    //匿名的闭包的类型是未知的，所以只有使用impl Trait才能返回一个闭包。
+    fn output_functions_test() {
+        fn create_fn() -> impl Fn() {
+            let text = "Fn".to_owned();
+
+            /*
+                除此之外，还必须使用 move 关键字，它表明所有的捕获都是通过值进行的。
+                这是必须的，因为在函数退出时，任何通过引用的捕获都被丢弃，在闭包中留下无效的引用。
+            
+             */
+            move || println!("This is a: {}", text)
+        }
+
+        fn create_fnmut() -> impl FnMut() {
+            let text = "FnMut".to_owned();
+            move || println!("This is a: {}", text)
+        }
+
+        fn create_fnonce() -> impl FnOnce() {
+            let text = "FnOnce".to_owned();
+            move || println!("This is a: {}", text)
+        }
         
+        let fn_plain = create_fn();
+        let mut fn_mut = create_fnmut();
+        let fn_once = create_fnonce();
+
+        fn_plain();
+        fn_mut();
+        fn_once();
     }
 
     fn examples_in_std_test() {
         //...Iterator::any
+        {   
+            /*
+            pub trait Iterator {
+                type Item;
+                fn any<F>(&mut self, f: F) -> bool
+                where F: FnMut(Self::Item) -> bool {}
+            }
+            */
+            let vec1 = vec![1, 2, 3];
+            let vec2 = vec![4, 5, 6];
+            println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
+            println!("2 in vec2: {}", vec2.into_iter().any(|x| x == 2));
+            
+            let arr1 = [1, 2, 3];
+            let arr2 = [4, 5, 6];
+            println!("2 in arr1: {}", arr1.iter().any(|&x| x == 2));
+            println!("2 in arr2: {}", arr2.into_iter().any(|x| x == 2));
+        }
 
         //...searching through iterators
-        
+        {   
+            /*
+            pub trait Iterator {
+                // 被迭代的类型。
+                type Item;
+
+                // `find` 接受 `&mut self` 参数，表明函数的调用者可以被借用和修改，
+                // 但不会被消耗。
+                fn find<P>(&mut self, predicate: P) -> Option<Self::Item> where
+                    // `FnMut` 表示被捕获的变量最多只能被修改，而不能被消耗。
+                    // `&Self::Item` 指明了被捕获变量的类型（译注：是对迭代器元素的引用类型）
+                    P: FnMut(&Self::Item) -> bool {}
+            }
+             */
+            let vec1 = vec![1, 2, 3];
+            let vec2 = vec![4, 5, 6];
+            let mut iter = vec1.iter();             //&i32
+            let mut into_iter = vec2.into_iter();   //i32
+            println!("Find 2 in vec1: {:?}", iter.find(|&&x| x == 2));
+            println!("Find 2 in vec2: {:?}", into_iter.find(|&x| x == 2));
+
+            let arr1 = [1, 2, 3];
+            let arr2 = [4, 5, 6];
+            println!("Find 2 in arr1: {:?}", arr1.iter().find(|&&x| x == 2));
+            println!("Find 2 in arr2: {:?}", arr2.into_iter().find(|&x| x == 2));       
+        }
     }
 }
 
 fn higher_order_functions_test() {
+    /*
+        HOF, 指那些输入一个或多个函数，并且/或者产生一个更有用的函数的函数
+     */
+    fn is_odd(n: u32) -> bool {
+        n % 2 == 1
+    }
+
+    println!("Find the sum of all the squared odd numbers under 1000");
+    let upper = 1000;
+    let mut acc = 0;
+    for n in 0.. {
+        let n_squared = n * n;
+        if n_squared >= upper {
+            break;
+        } else if is_odd(n_squared) {
+            acc += n_squared;
+        }
+    }
+    println!("imperative style: {}", acc);
     
+    //函数式写法
+    let sum_of_squared_odd_numbers: u32 =
+        (0..).map(|n| n * n)
+            .take_while(|&n| n < upper)
+            .filter(|&n| is_odd(n))
+            .fold(0, |acc, i| acc + i);
+    println!("functional style: {}", sum_of_squared_odd_numbers);
 }
 
 fn diverging_functions_test() {
+    fn foo() -> ! {
+        panic!("This call never returns.");
+    }
+
+    fn sum_odd_numbers(up_to: u32) -> u32 {
+        let mut acc = 0;
+        for i in 0..up_to {
+            let addition: u32 = match i % 2 == 1 {
+                true => i,
+                false => continue, //因为它永远不会返回，因此不会违反匹配表达式的类型要求
+            };
+            acc += addition;
+        }
+        acc
+    }
     
+    println!("Sum of odd numbers up to 9 (excluding): {}", sum_odd_numbers(9));
 }
