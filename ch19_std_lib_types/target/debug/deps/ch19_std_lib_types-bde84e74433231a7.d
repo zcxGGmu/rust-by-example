2f5cda62f8a5b16683acfb7c8100bed0bdcb4f56
Quan Zhou <zhouquan@iscas.ac.cn>
2024-08-02 19:41:41 +0800

    8.2 fuck shit

---
+++ ch19_std_lib_types/target/debug/deps/ch19_std_lib_types-bde84e74433231a7.d
@@ -0,0 +1,5 @@
+/home/zq/work-space/repo/patch-work/rust-projs/rust-by-example/ch19_std_lib_types/target/debug/deps/libch19_std_lib_types-bde84e74433231a7.rmeta: src/main.rs
+
+/home/zq/work-space/repo/patch-work/rust-projs/rust-by-example/ch19_std_lib_types/target/debug/deps/ch19_std_lib_types-bde84e74433231a7.d: src/main.rs
+
+src/main.rs:
